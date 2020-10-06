0fb7e10b9a4e9188a7dce05c9dc3ef14d7ebeadd
Vitalii Lazebnyi <>
2020-10-06 23:48:24 +0300

    Task 2. Part 8. Object getters and setters

---
+++ task_2/part_8_object_advanced.js
@@ -0,0 +1,40 @@
+// Создать объект human со следующими возможностями:
+//
+// 1. При записи значения в свойство fullName имя и фамилия
+// должны записываться в отдельные свойства firstName и lastName
+// 2. При чтении значения fullName оно должно получаться из
+// объединения firstName и lastName
+// 3. При задании значения свойства dateOfBirth должно так же
+// устанавливаться свойство age в зависимости от разницы года
+// рождения и текущего года
+
+// Date of birth - year only is expected.
+const person = {
+    firstName: '',
+    lastName:  '',
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+
+    set fullName(name) {
+        name = name.split(' ');
+        this.firstName = name[0];
+        this.lastName  = name[1];
+    },
+
+    get age() {
+        return new
+            Number(
+                (new Date().getTime()
+                        - this.dateOfBirth.getTime())
+                / 31536000000
+            )
+            .toFixed(0)
+    }
+};
+
+person.fullName = 'Vinni Pukh';
+person.dateOfBirth = new Date(1995, 0, 17);
+console.log(person.fullName);
+console.log(person.age);
