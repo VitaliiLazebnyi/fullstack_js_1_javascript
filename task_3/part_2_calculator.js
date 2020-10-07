e1ec4165dfac44ab2370130307faa10422992aa5
Vitalii Lazebnyi <>
2020-10-07 15:32:56 +0300

    Task 3. Part 2. Calculator

---
+++ task_3/part_2_calculator.js
@@ -0,0 +1,48 @@
+//
+// Кэширование
+//
+// Замыкания можно использовать для сохранения состояния
+// и дальнейшей работы с ним.
+//
+// Реализуйте функцию cache, которая вернет функцию, возводящую
+// число в степень и возвращающую результат.
+// Функция должна запоминать аргументы, которые она уже получала
+// и возвращать результат сразу, не вычисляя его повторно
+//
+// Пример:
+//   const calculate = cache();
+//
+// calculate(3, 3);  // { value: 27,   fromCache: false }
+// calculate(2, 10); // { value: 1024, fromCache: false }
+// calculate(2, 10); // { value: 1024, fromCache: true  }
+//
+
+function cache() {
+    this.store = {};
+
+    function execute(a, b) {
+        operation = `${a}*${b}`;
+
+        if (operation in this.store) {
+            return {
+                value: this.store[operation],
+                cache: true
+            };
+        }
+
+        this.store[operation] = a * b;
+
+        return {
+            value: this.store[operation],
+            cache: false
+        };
+    }
+
+    return execute;
+}
+
+calculator = cache();
+console.log(calculator)
+console.log(calculator(2,3));
+console.log(calculator(3,3));
+console.log(calculator(2,3));
