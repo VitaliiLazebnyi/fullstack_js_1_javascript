f9b984b1928aaf197141b21eda9b64dea9787871
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2020-10-07 15:15:49 +0300

    Task 3. Functions

---
+++ task_3/part_2_cache.js
@@ -0,0 +1,49 @@
+//
+// Кэширование
+//
+// Замыкания можно использовать для сохранения состояния
+// и дальнейшей работы с ним.
+//
+// Реализуйте функцию cache, которая вернет функцию, возводящую
+// число в степень и возвращающую результат.
+// Функция должна запоминать аргументы, которые она уже получала
+// и возвращать результат сразу, не вычисляя его повторно
+//
+// Пример:
+//   const calculate = cache();
+//
+// calculate(3, 3);  // { value: 27,   fromCache: false }
+// calculate(2, 10); // { value: 1024, fromCache: false }
+// calculate(2, 10); // { value: 1024, fromCache: true  }
+//
+
+function cache() {
+    let store = {};
+
+    const execute = (a, b) => {
+        let operation = `${a}*${b}`;
+
+        if (operation in store) {
+            return {
+                value: store[operation],
+                cache: true
+            };
+        }
+
+        store[operation] = a * b;
+
+        return {
+            value: store[operation],
+            cache: false
+        };
+    };
+
+    return execute;
+}
+
+calculator = cache();
+console.log(calculator)
+console.log(calculator(2,3));
+console.log(calculator(3,3));
+console.log(calculator(2,3));
+console.log(calculator.store);
