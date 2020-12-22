79ccf5161f60e1628c62dab9f7ef15a61a21e78f
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2020-12-22 19:24:06 +0200

    Test Calculator

--- task_10_testing/part_1/test/calculator.js
+++ task_10_testing/part_1/test/calculator.js
@@ -1,20 +1,39 @@
 const assert = require('assert');
-const calculator = require('../lib/calculator');
+const Calc = require('../lib/calculator');
 
-// const calculator = new Calc();
-// console.log(calculator.operation('31 + 32'));
-// calculator.addOperation('/', (a, b) => a / b);
-// console.log(calculator.operation('30 / 15'));
-// console.log(calculator.history());
-// calculator.clearHistory();
-// console.log(calculator.history());
+describe('Calc()', () => {
+    const calculator = new Calc();
 
+    it('can summ', () => {
+        assert.deepEqual(
+            calculator.operation('31 + 32'),
+            63
+        );
+    });
 
-describe('cache()', () => {
-  it('calculates, no cache', () => {
-      assert.deepEqual(
-          calculator(2,3),
-          { "cache": false, "value": 6 }
-          );
-  });
+    it('can provide custom operations', () => {
+        calculator.addOperation('/', (a, b) => a / b)
+        assert.deepEqual(
+            calculator.operation('30 / 15'),
+            2
+        );
+    });
+
+    it('can provide operations history', () => {
+        assert.deepEqual(
+            calculator.history(),
+            [
+                { operands: [ 31, 32 ], operation: '+' },
+                { operands: [ 30, 15 ], operation: '/' }
+            ]
+        );
+    });
+
+    it('can clear operations history', () => {
+        calculator.clearHistory()
+        assert.deepEqual(
+            calculator.history(),
+            [ ]
+        );
+    });
 });
